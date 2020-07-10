505337c20e868ecb4e0b37c6c85b4ed7c007f9de
danielkwon89 <danielkwon89@gmail.com>
2020-07-10 12:30:49 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
+require 'pry'
+
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    @@all = []
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        Pokemon.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.save(name, type, db_conn)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+
+        db_conn.execute(sql, name, type)
+    end
+
+    def self.find(id, db_conn)
+        pokemon_row =  db_conn.execute("SELECT * FROM pokemon WHERE id = ? LIMIT 1", id).flatten
+
+        name = pokemon_row[1]
+        type = pokemon_row[2]
+
+        pokemon_obj = self.new(id: id, name: name, type: type, db: db_conn)
+    end
 end
