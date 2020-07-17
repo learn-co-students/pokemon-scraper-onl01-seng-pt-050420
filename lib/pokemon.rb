5569bc3a864f4d813ae818a413bc67f36505cf74
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-17 14:29:50 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+    # Responsible for saving, adding, removing, 
+    # or changing anything about each Pokémon.
+
+    attr_accessor :id, :name, :type, :db 
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id 
+        @name = name 
+        @type = type 
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon_var = db.execute(
+        "SELECT * FROM pokemon 
+        WHERE id = ?", id).flatten
+
+        new_pokemon = Pokemon.new(id: id, name: pokemon_var[1], type: pokemon_var[2], db: db)
+
+        new_pokemon
+    end 
 end
